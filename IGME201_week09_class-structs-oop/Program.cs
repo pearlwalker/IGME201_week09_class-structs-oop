43fd418c65c4367a92e79e372480d8d62c72f188
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-10-26 17:25:40 -0400

    Add project files.

---
+++ IGME201_week09_class-structs-oop/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace IGME201_week09_class_structs_oop
+{
+    public class Character
+    {
+        public string name;
+        public int exp = 0;
+
+        //basic Character constructor
+        public Character()
+        {
+            name = "Not assigned";
+        }
+            //example of constructor overloading:
+        //Character constructor to take string param
+        public Character(string name)
+        {
+            this.name = name;
+        }
+        public void PrintStatsInfo()
+        {
+            Console.WriteLine("Hero: " + this.name + " - " + this.exp + " EXP");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare default Character instance
+            Character hero = new Character();
+            //Console.WriteLine("Hero: " + hero.name + " - " + hero.exp + " EXP");
+            hero.PrintStatsInfo();
+            
+            //Declare Character instance that's a girl
+            Character heroine = new Character("Agatha");
+            //Console.WriteLine("Hero: " + heroine.name + " - " + heroine.exp + " EXP");
+            heroine.PrintStatsInfo();
+        }
+    }
+    }
